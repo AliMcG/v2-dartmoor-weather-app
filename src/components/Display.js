b2c66df2f0cb7f21e1d55ca995e9c8f8f1979ef4
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-09-15 21:44:29 +0100

    sorts icons into an array an returns the most common string for fetch ison

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,11 +2,11 @@ import React, { useState } from "react"
 import BarChart from "./BarChart"
 
 export default function Display({day0}) {
-  console.log(day0)
+  
   // Uses an array of the days of the week to return the day of the week
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const day = days[new Date(day0[0].dt * 1000).getDay()];
-  console.log(day)
+  // console.log(day)
 
   // This returns the the number of current day in the moth = i.e 14
   // const date = new Date(day0[0].dt * 1000).getDate()
@@ -16,7 +16,7 @@ export default function Display({day0}) {
     {labels: day0.map((day) => new Date(day.dt * 1000).getHours()),
     datasets: [
     {
-      label: `${day}`,
+      label: `Temperture`,
       data: day0.map((day) => day.main.temp),
       backgroundColor: [
         'rgba(127, 175, 219, 1)'],
@@ -24,21 +24,46 @@ export default function Display({day0}) {
     }]}
   )
 
-  
+  // map or filter through day0 to find the highes
+  // map throught the icons and return the most common value.
+  function mostFreqStr(arr) {
+    var obj = {}, mostFreq = 0, which = [];
 
-  // this only returns the data for the [0] in the array - so the 1st info for the day.
-  const icon = day0[0].weather[0].icon
+    arr.forEach(ea => {
+      if (!obj[ea]) {
+        obj[ea] = 1;
+      } else {
+        obj[ea]++;
+      }
+
+      if (obj[ea] > mostFreq) {
+        mostFreq = obj[ea];
+        which = [ea];
+      } else if (obj[ea] === mostFreq) {
+        which.push(ea);
+      }
+    });
+
+    return which;
+  }
+  
+  const iconList = day0.map((day) => day.weather[0].icon)
   const description = day0[0].weather[0].description
+
+  const icon = mostFreqStr(iconList)
+  console.log(icon)
+
   const tempCheck = day0.map((day) => day.main.temp_max)
   const maxTemp = Math.round(Math.max(...tempCheck))
-  // date_text, temp, icon, description
+  
 
   return (
     <div className="day-card">
-      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon" />
+      <img src={`http://openweathermap.org/img/wn/${icon[0]}@2x.png`} alt="icon" />
       <p>{description}</p>
       <BarChart ChartData={tempData} />
       <p>Max Temperture: {maxTemp}°C</p>
+      <p>{day}</p>
     </div>
   );
 }
